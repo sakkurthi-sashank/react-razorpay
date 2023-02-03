55218a5a0d78bcb597ea93e26c3014188ebd0262
sakkurthisashank <sakkurthisashank@gmail.com>
2023-02-04 00:26:54 +0530

    feat: payment invoke using firebase functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,63 @@
+import Script from "next/script";
+import axios from "axios";
+
+declare global {
+  interface Window {
+    Razorpay: any;
+  }
+}
+
 export default function Home() {
-  return <></>;
+  const getDataFromServer = async () => {
+    const { data } = await axios.post(
+      "https://us-central1-react-razorpay.cloudfunctions.net/createOrder",
+      {
+        amount: 1,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return data;
+  };
+
+  const handlePayment = () => {
+    getDataFromServer().then((data) => {
+      const options = {
+        key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+        currency: data.currency,
+        amount: data.amount,
+        order_id: data.id,
+        name: "Sample Transaction",
+        description: "Test Transaction",
+        prefill: {
+          name: "sakkurthisashank",
+          email: "sakkurthisashank@gmail.com",
+          contact: "9999999999",
+        },
+        notes: {
+          address: "",
+        },
+        theme: {
+          color: "#3399cc",
+        },
+      };
+      const razorpay = new window.Razorpay(options);
+      razorpay.open();
+    });
+  };
+
+  return (
+    <>
+      <Script
+        src="https://checkout.razorpay.com/v1/checkout.js"
+        strategy="lazyOnload"
+      />
+      <div>
+        <button onClick={handlePayment}>Pay</button>
+      </div>
+    </>
+  );
 }
